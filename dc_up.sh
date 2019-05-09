1776647853c09e3478ce8879d6aba56ad674e5f4
gomzaemon111 <goki14914@icloud.com>
2019-05-10 00:39:19 +0900

    add dc_up.sh

---
+++ dc_up.sh
@@ -0,0 +1,19 @@
+if [ "$1" == "build" ]
+then
+  echo "docker-compose buildから実行します"
+  docker-compose build
+elif [ "$1" == "" ]
+then
+  echo "docker-compose up -d から実行します"
+else
+  echo "引数は nil か \"build\"のいずれかが必要です"
+  exit
+fi
+
+bundle install -j4
+bundle exec docker-sync start
+dcoker-compose up -d
+docker ps -a
+echo "WaffleのDocker環境構築が終わりました！"
+echo "これからbashでlifeコンテナの中に入ります"
+docker-compose exec life bash
